252f1cd821aa8c76755cd0f5a234d3567f6e8aa6
Carlo Wood <carlo@alinoe.com>
2026-06-15 01:21:36 +0200

    Replace magic_enum with enchantum.

--- System.Error/Errors.cxx
+++ System.Error/Errors.cxx
@@ -1,24 +1,15 @@
 #include "sys.h"
 #include "Errors.h"
 
-#include <magic_enum.hpp>
+#include <enchantum/enchantum.hpp>
 #include <iostream>
-
-// Specialization because the enum values are larger than MAGIC_ENUM_RANGE_MAX (128).
-// See https://github.com/Neargye/magic_enum/blob/master/doc/limitations.md
-template<>
-struct magic_enum::customize::enum_range<dbus::errors::System::Error::Errors>
-{
-  static constexpr int min = 1;
-  static constexpr int max = dbus::errors::System::Error::SE_MAGIC_ENUM_RANGE_MAX;
-};
 
 namespace dbus::errors {
 namespace System::Error {
 
 std::string to_string(Errors error)
 {
-  auto sv = magic_enum::enum_name(error);
+  auto sv = enchantum::to_string(error);
   sv.remove_prefix(3);  // Remove "SE_" from the name (see generate_SystemErrors.sh).
   return std::string{sv};
 }
@@ -31,7 +22,7 @@ std::ostream& operator<<(std::ostream& os, Errors error)
 
 std::error_code ErrorDomain::get_error_code(std::string const& member_name) const
 {
-  return make_error_code(*magic_enum::enum_cast<Errors>("SE_" + member_name));
+  return make_error_code(*enchantum::cast<Errors>("SE_" + member_name));
 }
 
 // Instantiation of the error category object.
--- tests/org.sdbuscpp.Concatenator.Error/Errors.cxx
+++ tests/org.sdbuscpp.Concatenator.Error/Errors.cxx
@@ -1,6 +1,6 @@
 #include "sys.h"
 #include "Errors.h"
-#include <magic_enum.hpp>
+#include <enchantum/enchantum.hpp>
 #include <iostream>
 
 namespace dbus::errors {
@@ -8,7 +8,7 @@ namespace org::sdbuscpp::Concatenator::Error {
 
 std::string to_string(Errors error)
 {
-  return std::string{magic_enum::enum_name(error)};
+  return std::string{enchantum::to_string(error)};
 }
 
 std::ostream& operator<<(std::ostream& os, Errors error)
@@ -19,7 +19,7 @@ std::ostream& operator<<(std::ostream& os, Errors error)
 
 std::error_code ErrorDomain::get_error_code(std::string const& member_name) const
 {
-  auto error = magic_enum::enum_cast<Errors>(member_name);
+  auto error = enchantum::cast<Errors>(member_name);
   return error.has_value() ? make_error_code(*error) : s_unknown_error;
 }
 
